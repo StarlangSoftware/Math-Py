3cd3a1cb4b54368a25f2f90e94c56b019d838516
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-30 19:32:32 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name='NlpToolkit-Math',
-    version='1.0.14',
+    version='1.0.15',
     packages=['Math'],
     url='https://github.com/StarlangSoftware/Math-Py',
     license='',
     author='olcaytaner',
     author_email='olcay.yildiz@ozyegin.edu.tr',
-    description='Math library'
+    description='Math library',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
